8694c8814f382306454af4eb58d4601fdc5fe40d
Caleb Han <calebhan08js@gmail.com>
2023-09-06 17:09:47 +0800

    added cmd prompts

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -11,7 +11,18 @@ function ChatBot() {
   };
 
   const handleNewUserMessage = (newMessage) => {
-    addResponseMessage("Hi there! How may I assist you today?");
+    if (newMessage == "Hi") {
+      addResponseMessage("Hi there! How may I assist you today?");
+    }
+    else if (newMessage == "I'm feeling lonely") {
+      addResponseMessage("I'm sorry you feel that way. I'm here to chat and keep you company! Loneliness can be a tough feeling to deal with, but talking to someone or engaging in activities you enjoy can help alleviate it. Is there something specific you'd like to talk about or do to help you feel better?")
+    }
+    else if (newMessage == "I am having a hard time taking care of my patients") {
+      addResponseMessage("I'm truly sorry to hear about your dog. Losing a beloved pet can be very painful. If you'd like to talk or share memories, I'm here to listen and provide support. Remember, it's okay to grieve, and reaching out to friends and family can also help.")
+    }
+    else {
+      addResponseMessage("Hi there! How may I assist you today?")
+    }
   };
 
   return (
