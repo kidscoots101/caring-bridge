6a5891492e319a8e4d75401283c29e79fefd0f52
Caleb Han <calebhan08js@gmail.com>
2023-09-06 16:18:32 +0800

    added resources

--- src/screens/User.js
+++ src/screens/User.js
@@ -36,35 +36,54 @@ export default function MentalHealth() {
       </header>
 
       <section className="resources-section">
-        <h2>Mental Health Resources</h2>
-        <ul className="resource-list">
-          <li>
-            <a href="#">Resource 1</a>
-          </li>
-          <li>
-            <a href="#">Resource 2</a>
-          </li>
-          <li>
-            <a href="#">Resource 3</a>
-          </li>
-        </ul>
-      </section>
-      <section className="stories-section">
-  <h2>Stories of Mental Health Survivors</h2>
-  <div className="story-cards">
-    {mentalHealthStories.map((story, index) => (
-      <div key={index} className="story-card">
-        <div style={{flexDirection: 'row'}}>
-        <img src={story.imageSrc} alt={story.title} />
-        <p>{story.content}</p>
-        </div>
-        <a href={story.link} target="_blank" rel="noopener noreferrer" className="read-more-button">
-          Read More
-        </a>
-      </div>
-    ))}
-  </div>
+  <h2 className="resource-heading">Mental Health Resources</h2>
+  <ul className="resource-list">
+    <li className="resource-item">
+      <a href="https://www.nami.org/Home" target="_blank" rel="noopener noreferrer" className="resource-link">
+        National Alliance on Mental Illness (NAMI)
+      </a>
+    </li>
+    <li className="resource-item">
+      <a href="https://www.psychologytoday.com/us" target="_blank" rel="noopener noreferrer" className="resource-link">
+        Find a Therapist on Psychology Today
+      </a>
+    </li>
+    <li className="resource-item">
+      <a href="https://www.crisistextline.org/" target="_blank" rel="noopener noreferrer" className="resource-link">
+        Crisis Text Line
+      </a>
+    </li>
+    <li className="resource-item">
+      <a href="https://www.samhsa.gov/find-help/national-helpline" target="_blank" rel="noopener noreferrer" className="resource-link">
+        SAMHSA National Helpline
+      </a>
+    </li>
+    <li className="resource-item">
+      <a href="https://www.mentalhealth.gov/get-help/immediate-help" target="_blank" rel="noopener noreferrer" className="resource-link">
+        Immediate Help for Mental Health
+      </a>
+    </li>
+  </ul>
 </section>
+
+
+      
+            <section className="stories-section">
+        <h2>Stories of Mental Health Survivors</h2>
+        <div className="story-cards">
+          {mentalHealthStories.map((story, index) => (
+            <div key={index} className="story-card">
+              <div style={{flexDirection: 'row'}}>
+              <img src={story.imageSrc} alt={story.title} />
+              <p>{story.content}</p>
+              </div>
+              <a href={story.link} target="_blank" rel="noopener noreferrer" className="read-more-button">
+                Read More
+              </a>
+            </div>
+          ))}
+        </div>
+      </section>
 
 
       <section className="relaxation-section">
