5f65554da550fcd035474dbed9efcb8e7b9df1e3
Caleb Han <calebhan08js@gmail.com>
2023-09-10 11:08:36 +0800

    logo

--- src/screens/Care.js
+++ src/screens/Care.js
@@ -14,6 +14,7 @@ import golden from "../assets/golden.png";
 import maps from "../assets/google-maps-icon.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar, faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
+import logo from '../assets/caring-bridge-icon.jpeg'
 
 Modal.setAppElement("#root");
 
@@ -375,7 +376,6 @@ export default function Care() {
         </div>
       </div>
       </div>
-
       {showMentalHealthResource ? (
         <>
           <User />
