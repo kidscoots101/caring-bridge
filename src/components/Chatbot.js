5df869b8936161508263ea7731e174f806ab643e
Caleb Han <calebhan08js@gmail.com>
2023-09-11 21:05:57 +0800

    welcome message

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState, useEffect} from "react";
 import { Widget, addResponseMessage } from "react-chat-widget";
 import "react-chat-widget/lib/styles.css";
 import "./Chat.css";
@@ -17,25 +17,28 @@ function ChatBot() {
 
 
   const handleNewUserMessage = (newMessage) => {
-    if (newMessage === "Hi") {
+    if (!newMessage || newMessage.trim() === "") {
+      addResponseMessage("Welcome! How can I assist you today?");
+    } else if (newMessage === "Hi") {
       addResponseMessage("Hi there! How may I assist you today?");
     } else if (newMessage === "I'm feeling lonely") {
       addResponseMessage(
-        "I'm sorry you feel that way. I'm here to chat and keep you company! Loneliness can be a tough feeling to deal with, but talking to someone or engaging in activities you enjoy can help alleviate it. Is there something specific you'd like to talk about or do to help you feel better?",
+        "I'm sorry you feel that way. I'm here to chat and keep you company! Loneliness can be a tough feeling to deal with, but talking to someone or engaging in activities you enjoy can help alleviate it. Is there something specific you'd like to talk about or do to help you feel better?"
       );
     } else if (
       newMessage === "I am having a hard time taking care of my patients"
     ) {
       addResponseMessage(
-        "I'm sorry to hear that you're having a difficult time taking care of your patients. Providing care for others, especially in a healthcare or caregiving setting, can be emotionally and physically demanding. If you'd like to talk or share anything, I'm here to listen and provide support. Remember, it's okay to struggle, reaching out to friends and family can also help.",
+        "I'm sorry to hear that you're having a difficult time taking care of your patients. Providing care for others, especially in a healthcare or caregiving setting, can be emotionally and physically demanding. If you'd like to talk or share anything, I'm here to listen and provide support. Remember, it's okay to struggle, reaching out to friends and family can also help."
       );
     } else if (newMessage === "hsa") {
-      addResponseMessage("I'm sorry I don't understand.")
-    }
-    else {
+      addResponseMessage("I'm sorry I don't understand.");
+    } else {
       addResponseMessage("Hi there! How may I assist you today?");
     }
   };
+  
+
   return (
     <div
       className={`${isChatOpen ? "expanded" : ""}`}
