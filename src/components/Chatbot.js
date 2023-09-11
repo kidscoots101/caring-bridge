bf577251152cf47592d1b9ff18056bca3cd411e7
Caleb Han <calebhan08js@gmail.com>
2023-09-11 21:12:15 +0800

    added more user msgs

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -20,21 +20,29 @@ function ChatBot() {
       addResponseMessage("Hi there! How may I assist you today?");
     } else if (newMessage === "I'm feeling lonely") {
       addResponseMessage(
-        "I'm sorry you feel that way. I'm here to chat and keep you company! Loneliness can be a tough feeling to deal with, but talking to someone or engaging in activities you enjoy can help alleviate it. Is there something specific you'd like to talk about or do to help you feel better?",
+        "I'm sorry you feel that way. I'm here to chat and keep you company! Loneliness can be a tough feeling to deal with, but talking to someone or engaging in activities you enjoy can help alleviate it. Is there something specific you'd like to talk about or do to help you feel better?"
       );
     } else if (
       newMessage === "I am having a hard time taking care of my patients"
     ) {
       addResponseMessage(
-        "I'm sorry to hear that you're having a difficult time taking care of your patients. Providing care for others, especially in a healthcare or caregiving setting, can be emotionally and physically demanding. If you'd like to talk or share anything, I'm here to listen and provide support. Remember, it's okay to struggle, reaching out to friends and family can also help.",
+        "I'm sorry to hear that you're having a difficult time taking care of your patients. Providing care for others, especially in a healthcare or caregiving setting, can be emotionally and physically demanding. If you'd like to talk or share anything, I'm here to listen and provide support. Remember, it's okay to struggle, reaching out to friends and family can also help."
       );
-    } else if (newMessage === "hsa") {
-      addResponseMessage("I'm sorry I don't understand.")
-    }
-    else {
+    } else if (newMessage === "I'm feeling stressed") {
+      addResponseMessage(
+        "I understand that you're feeling stressed. Stress is a common experience, and there are various relaxation techniques you can try. You can practice deep breathing, progressive muscle relaxation, or mindfulness meditation to help reduce stress. Our website shows more!"
+      );
+    } else if (newMessage === "Tell me a joke") {
+      addResponseMessage(
+        "Sure, here's a joke for you: Why don't scientists trust atoms? Because they make up everything!"
+      );
+    } else if (newMessage === "Thank you") {
+      addResponseMessage("You're welcome! If you have any more questions, feel free to ask.");
+    } else {
       addResponseMessage("Hi there! How may I assist you today?");
     }
   };
+  
 
   return (
     <div
