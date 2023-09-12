cbd0b6821eb7854e905edcb643ba58fc876587a8
Caleb Han <calebhan08js@gmail.com>
2023-09-12 20:20:48 +0800

    removed case sensitivity

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -16,32 +16,33 @@ function ChatBot() {
   };
 
   const handleNewUserMessage = (newMessage) => {
-    if (newMessage === "Hi") {
+    const lowerCaseMessage = newMessage.toLowerCase();
+  
+    if (lowerCaseMessage === "hi") {
       addResponseMessage("Hi there! How may I assist you today?");
-    } else if (newMessage === "I'm feeling lonely") {
+    } else if (lowerCaseMessage === "i'm feeling lonely") {
       addResponseMessage(
         "I'm sorry you feel that way. I'm here to chat and keep you company! Loneliness can be a tough feeling to deal with, but talking to someone or engaging in activities you enjoy can help alleviate it. Is there something specific you'd like to talk about or do to help you feel better?"
       );
-    } else if (
-      newMessage === "I am having a hard time taking care of my patients"
-    ) {
+    } else if (lowerCaseMessage === "i am having a hard time taking care of my patients") {
       addResponseMessage(
         "I'm sorry to hear that you're having a difficult time taking care of your patients. Providing care for others, especially in a healthcare or caregiving setting, can be emotionally and physically demanding. If you'd like to talk or share anything, I'm here to listen and provide support. Remember, it's okay to struggle, reaching out to friends and family can also help."
       );
-    } else if (newMessage === "I'm feeling stressed") {
+    } else if (lowerCaseMessage === "i'm feeling stressed") {
       addResponseMessage(
         "I understand that you're feeling stressed. Stress is a common experience, and there are various relaxation techniques you can try. You can practice deep breathing, progressive muscle relaxation, or mindfulness meditation to help reduce stress. Our website shows more!"
       );
-    } else if (newMessage === "Tell me a joke") {
+    } else if (lowerCaseMessage === "tell me a joke") {
       addResponseMessage(
         "Sure, here's a joke for you: Why don't scientists trust atoms? Because they make up everything!"
       );
-    } else if (newMessage === "Thank you") {
+    } else if (lowerCaseMessage === "thank you") {
       addResponseMessage("You're welcome! If you have any more questions, feel free to ask.");
     } else {
       addResponseMessage("Sorry I don't really understand. Could you repeat again?");
     }
   };
+  
   
 
   return (
